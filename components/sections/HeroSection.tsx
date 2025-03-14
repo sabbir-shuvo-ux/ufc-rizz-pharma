857102fed8d1d3b6ac32d24cd1a043b4c6dfe42e
sabbir-shuvo-ux <shuvosabbir134@gmail.com>
2025-03-14 22:04:56 +0600

    updated hero section title with sectionTitle component

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -1,15 +1,15 @@
 import { Button } from "@/components/ui/button";
+import SectionTitle from "@/components/ui/SectionTitle";
 
 const HeroSection = () => {
   return (
     <section className="container">
       <div className="max-w-[547px] pt-[15rem] flex gap-[2.12rem] flex-col">
-        <h2 className="text-white font-impact text-[5rem] leading-24 capitalize">
-          Prescription treatments for your{" "}
-          <span className="bg-gradient-to-b from-[#C1842D] to-[#ECC974] bg-clip-text text-transparent block">
-            health goals
-          </span>
-        </h2>
+        <SectionTitle
+          beforeText="Prescription treatments for your"
+          gradientText="health goals"
+          className="text-[5rem] leading-24"
+        />
 
         <Button variant={"secondary"} size={"secondary"}>
           Find my treatment
